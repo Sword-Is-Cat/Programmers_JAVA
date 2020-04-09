36f92c0c122b62482d52864aa6e4305207761758
KOSTA <KOSTA@KOSTA-HP>
2020-04-09 10:35:47 +0900

    Modify to use LickedList

--- Programmers_JAVA/src/level2/ex다리를지나는트럭/Solution.java
+++ Programmers_JAVA/src/level2/ex다리를지나는트럭/Solution.java
@@ -3,29 +3,30 @@ package level2.ex다리를지나는트럭;
  * https://programmers.co.kr/learn/courses/30/lessons/42583
  */
 
-import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class Solution {
 	public int solution(int bridge_length, int weight, int[] truck_weights) {
 
 		int second = 0;
 		int sum = 0;
-		ArrayDeque<Integer> bridge = new ArrayDeque<>();
+		Queue<Integer> bridge = new LinkedList<>();
 		for (int i = 0; i < bridge_length; i++) {
-			bridge.push(0);
+			bridge.add(0);
 		}
 		
 		int truck = next(truck_weights);
 		
 		do {
-			sum -= bridge.pollLast();
+			sum -= bridge.poll();
 			second++;
 			if(sum+truck<=weight) {
 				sum += truck;
-				bridge.push(truck);
+				bridge.add(truck);
 				truck = next(truck_weights);
 			}else {
-				bridge.push(0);
+				bridge.add(0);
 			}
 		} while (sum > 0);
 
