533141d5d71e314618d1b3d5154e26946ea5c775
KOSTA <KOSTA@192.168.0.113>
2020-04-09 11:24:32 +0900

    update

--- Programmers_JAVA/src/level2/ex점프와순간이동/Solution.java
+++ Programmers_JAVA/src/level2/ex점프와순간이동/Solution.java
@@ -6,16 +6,11 @@ package level2.ex점프와순간이동;
 public class Solution {
     public int solution(int n) {
         int ans = 0;
-
         while(n>0) {
-        	if(n%2==0)
-        		n = n/2;
-        	else {
-        		n = n-1;
+        	if(n%2==1)
         		ans++;
-        	}
+        	n/=2;
         }
-
         return ans;
     }
 }
