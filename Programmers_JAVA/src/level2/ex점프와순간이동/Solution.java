0031fa01f1abe3e8e5e19f13474270be67281808
KOSTA <KOSTA@192.168.0.113>
2020-04-09 11:21:29 +0900

    solve

---
+++ Programmers_JAVA/src/level2/ex점프와순간이동/Solution.java
@@ -0,0 +1,21 @@
+package level2.ex점프와순간이동;
+/*
+ * https://programmers.co.kr/learn/courses/30/lessons/12980
+ */
+
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+
+        while(n>0) {
+        	if(n%2==0)
+        		n = n/2;
+        	else {
+        		n = n-1;
+        		ans++;
+        	}
+        }
+
+        return ans;
+    }
+}
