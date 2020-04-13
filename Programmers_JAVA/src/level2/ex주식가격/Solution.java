b050a5f7ca32ba8e0bfb817dde591b2eb62ddf22
KOSTA <KOSTA@KOSTA-HP>
2020-04-13 10:21:31 +0900

    solve

---
+++ Programmers_JAVA/src/level2/ex주식가격/Solution.java
@@ -0,0 +1,20 @@
+package level2.ex주식가격;
+/*
+ * https://programmers.co.kr/learn/courses/30/lessons/42584
+ */
+
+class Solution {
+	public int[] solution(int[] prices) {
+		int[] answer = new int[prices.length];
+		for (int i = 0; i < prices.length; i++) {
+			for (int j = 0; i + j < prices.length; j++) {
+				if (prices[i] > prices[i + j]) {
+					answer[i] = j;
+					break;
+				} else if (i + j == prices.length - 1)
+					answer[i] = j;
+			}
+		}
+		return answer;
+	}
+}
