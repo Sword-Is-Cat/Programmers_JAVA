f0418eaa72c349234554d5c32c452a656b59e3fd
HongBum(home) <jhbsp@naver.com>
2020-05-11 23:43:04 +0900

    resolve

--- Programmers_JAVA/src/level1/ex최대공약수와최소공배수/Solution.java
+++ Programmers_JAVA/src/level1/ex최대공약수와최소공배수/Solution.java
@@ -1,41 +1,21 @@
 package level1.ex최대공약수와최소공배수;
 
-
 /*
- * ���� ����
-�� ���� �Է¹޾� �� ���� �ִ������� �ּҰ������ ��ȯ�ϴ� �Լ�, solution�� �ϼ��� ������. 
-�迭�� �� �տ� �ִ�����, �״��� �ּҰ������ �־� ��ȯ�ϸ� �˴ϴ�. 
-���� ��� �� �� 3, 12�� �ִ������� 3, �ּҰ������ 12�̹Ƿ� solution(3, 12)�� [3, 12]�� ��ȯ�ؾ� �մϴ�.
-
-���� ����
-�� ���� 1�̻� 1000000������ �ڿ����Դϴ�.
+ * https://programmers.co.kr/learn/courses/30/lessons/12940
  */
 
 class Solution {
 	public int[] solution(int n, int m) {
 
-		int gcd = 0;
-		int multi = n*m;
+		int gcd = gcd(n, m);
+		int lcm = n * m / gcd;
 
-		while (gcd == 0) {
-
-			if (n > m) {
-				int temp = m;
-				m = n;
-				n = temp;
-			}
-
-			if (m % n == 0) {
-				gcd = n;
-			}
-
-			m -= n;
-
-		}
-
-		int lcm = multi / gcd;
 		int[] answer = { gcd, lcm };
 
 		return answer;
+	}
+
+	public int gcd(int n, int m) {
+		return (n % m == 0) ? m : gcd(m, n % m);
 	}
 }
