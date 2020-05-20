d5c77cb300852b5fcf912ee772a2c245a92d6e9b
HongBum(KOSTA) <jhbsp@naver.com>
2020-05-20 13:39:47 +0900

    solve

---
+++ Programmers_JAVA/src/level2/ex카펫/Solution.java
@@ -0,0 +1,26 @@
+package level2.ex카펫;
+
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/42842
+ */
+
+class Solution {
+	public int[] solution(int brown, int yellow) {
+
+		/* garo, sero 는 각각 노란 칸의 가로의길이, 세로의길이 */
+		int garo = 0;
+		int sero = 0;
+
+		for (int i = 1; i <= brown / 2 - 2; i++) {
+			if (i * (brown / 2 - 2 - i) == yellow) {
+				sero = i;
+				break;
+			}
+		}
+
+		garo = brown / 2 - 2 - sero;
+
+		int[] answer = { garo + 2, sero + 2 };
+		return answer;
+	}
+}
