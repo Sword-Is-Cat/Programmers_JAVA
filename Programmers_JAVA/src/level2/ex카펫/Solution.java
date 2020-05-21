b79e0542250a1eeee8ded0dc17b0291cdccd5407
장홍범 <62692449+Sword-Is-Cat@users.noreply.github.com>
2020-05-21 09:53:13 +0900

    Update Solution.java

--- Programmers_JAVA/src/level2/ex카펫/Solution.java
+++ Programmers_JAVA/src/level2/ex카펫/Solution.java
@@ -11,7 +11,7 @@ class Solution {
 		int garo = 0;
 		int sero = 0;
 
-		for (int i = 1; i <= brown / 2 - 2; i++) {
+		for (int i = 1; i < brown / 4; i++) {
 			if (i * (brown / 2 - 2 - i) == yellow) {
 				sero = i;
 				break;
