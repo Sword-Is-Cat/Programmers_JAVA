32386814b997bf0ec3d31d3d39db95ba2e7393bc
HongBum(home) <jhbsp@naver.com>
2020-05-25 03:15:18 +0900

    update

--- Programmers_JAVA/src/level2/exHindex/Solution.java
+++ Programmers_JAVA/src/level2/exHindex/Solution.java
@@ -5,23 +5,17 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] citations) {
     	
-    	int answer = 0;
+    	int answer = citations.length;
     	
     	Arrays.sort(citations);
-    	
-    	boolean flag = true;
     	
     	for(int i = 0 ; i<citations.length ; i++) {
     		
     		if(citations[citations.length-1-i]<i+1) {
     			answer = i;
-    			flag = false;
     			break;
     		}
     	}
-    	
-    	if(flag)
-    		answer = citations.length;
     	
         return answer;
     }
