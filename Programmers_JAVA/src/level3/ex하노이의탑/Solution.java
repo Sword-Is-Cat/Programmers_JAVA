a974b2f33a64afce0108463eebbb4791fe2bd0c4
HongBum(home) <jhbsp@naver.com>
2020-05-25 02:30:26 +0900

    solve

---
+++ Programmers_JAVA/src/level3/ex하노이의탑/Solution.java
@@ -0,0 +1,40 @@
+package level3.ex하노이의탑;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+
+	List<int[]> list;
+
+	public int[][] solution(int n) {
+
+		list = new ArrayList<>();
+
+		hanoi(n, 1, 3);
+
+		int[][] answer = new int[list.size()][];
+
+		for (int i = 0; i < list.size(); i++)
+			answer[i] = list.get(i);
+
+		return answer;
+	}
+
+	public void hanoi(int n, int from, int to) {
+
+		int[] arr = { from, to };
+
+		if (n == 1) {
+			list.add(arr);
+		} else {
+
+			hanoi(n - 1, from, 6 - from - to);
+			list.add(arr);
+			hanoi(n - 1, 6 - from - to, to);
+
+		}
+
+	}
+
+}
