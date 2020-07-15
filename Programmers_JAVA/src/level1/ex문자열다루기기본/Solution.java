9cccf51b37258daf38705feb3a26e71f3348e9bb
HongBum(home) <jhbsp@naver.com>
2020-07-15 21:37:06 +0900

    update

--- Programmers_JAVA/src/level1/ex문자열다루기기본/Solution.java
+++ Programmers_JAVA/src/level1/ex문자열다루기기본/Solution.java
@@ -1,6 +1,5 @@
 package level1.ex문자열다루기기본;
 
-
 /*
  * ���� ����
 ���ڿ� s�� ���̰� 4 Ȥ�� 6�̰�, ���ڷθ� �������ִ��� Ȯ�����ִ� �Լ�, solution�� �ϼ��ϼ���. 
@@ -15,7 +14,27 @@ class Solution {
 
 		boolean b1 = (s.length() == 4 || s.length() == 6);
 		boolean b2 = (s.replaceAll("[0-9]", "").length() == 0);
-		
+
 		return (b1 & b2);
+	}
+}
+
+class Solution2 {
+	public boolean solution(String s) {
+		boolean answer = true;
+
+		char[] arr = s.toCharArray();
+
+		if (arr.length == 4 || arr.length == 6) {
+			for (char ch : arr) {
+				if (ch < 48 || ch > 57) {
+					answer = false;
+					break;
+				}
+			}
+		} else
+			answer = false;
+
+		return answer;
 	}
 }
