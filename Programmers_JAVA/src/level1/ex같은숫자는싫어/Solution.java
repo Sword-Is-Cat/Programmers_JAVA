3222313deebad2a0328edf36513dd6162721bdaf
HongBum(home) <jhbsp@naver.com>
2020-07-15 21:09:24 +0900

    solve

--- Programmers_JAVA/src/level1/ex같은숫자는싫어/Solution.java
+++ Programmers_JAVA/src/level1/ex같은숫자는싫어/Solution.java
@@ -2,29 +2,30 @@ package level1.ex같은숫자는싫어;
 
 /* https://programmers.co.kr/learn/courses/30/lessons/12906 */
 
-import java.util.*;
+import java.util.ArrayList;
 
 public class Solution {
-		public int[] solution(int []arr) {
-        int[] answer = {};
-        
-        ArrayList<Integer> list = new ArrayList<>();
-        
-        list.add(arr[0]);
-        
-        for(int i = 1; i<arr.length ; i++) {
-        	
-        	if(arr[i-1] != arr[i])
-        		list.add(arr[i]);
-        }
-        
-        answer = new int[list.size()];
-        
-        for(int i = 0 ; i<list.size() ; i++) {
-        	
-        	answer[i] = list.get(i);
-        }
-        
-        return answer;
+	public int[] solution(int[] arr) {
+
+		ArrayList<Integer> list = new ArrayList<>();
+
+		int temp = -1;
+		
+		for (int i : arr) {
+			if (temp == i)
+				continue;
+			else {
+				list.add(i);
+				temp = i;
+			}
+		}
+
+		int[] answer = new int[list.size()];
+
+		for (int i = 0; i < answer.length; i++) {
+			answer[i] = list.get(i);
+		}
+
+		return answer;
 	}
 }
