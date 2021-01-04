9cab038ecb2b5d29e462631fd8405bd3f4a9a3ee
jang <jhbsp@naver.com>
2021-01-05 00:46:00 +0900

    solve

---
+++ Programmers_JAVA/src/level1/ex내적/Solution.java
@@ -0,0 +1,15 @@
+package level1.ex내적;
+
+class Solution {
+	public int solution(int[] a, int[] b) {
+
+		int answer = 0;
+
+		for (int i = 0; i < a.length; i++) {
+			answer += a[i] * b[i];
+		}
+
+		return answer;
+
+	}
+}
