4a5b3119f0baee89f3008a505902450cd6086ad7
jang <jhbsp@naver.com>
2022-03-18 10:16:29 +0900

    220318

---
+++ Programmers_JAVA/src/level2/ex모음사전/Solution.java
@@ -0,0 +1,31 @@
+package level2.ex모음사전;
+
+class Solution {
+    public int solution(String word) {
+
+        word = word.replace('A', '1').replace('E', '2').replace('I', '3').replace('O', '4').replace('U', '5');
+
+        char[] chArr = word.toCharArray();
+
+        int answer = 0;
+
+        for (int i = 0; i < chArr.length; i++) {
+
+            answer += (chArr[i] - 49) * cntNthLengthWords(4 - i) + 1;
+
+        }
+
+        return answer;
+    }
+
+    public int cntNthLengthWords(int length) {
+
+        int sum = 0;
+
+        for (int i = 0; i < length; i++) {
+            sum += Math.pow(5, i + 1);
+        }
+        return sum + 1;
+    }
+
+}
