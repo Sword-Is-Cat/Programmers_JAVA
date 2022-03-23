c798ea16eef0ceee36f8b722a0e87a825152cad6
jang <jhbsp@naver.com>
2022-03-23 16:55:45 +0900

    220323 refactoring

--- Programmers_JAVA/src/level1/ex정수내림차순으로배치하기/Solution.java
+++ Programmers_JAVA/src/level1/ex정수내림차순으로배치하기/Solution.java
@@ -1,30 +1,12 @@
 package level1.ex정수내림차순으로배치하기;
 
-
-/*
- * ���� ����
-�Լ� solution�� ���� n�� �Ű������� �Է¹޽��ϴ�. n�� �� �ڸ����� ū�ͺ��� ���� ������ ������
-���ο� ������ �������ּ���. ������� n�� 118372�� 873211�� �����ϸ� �˴ϴ�.
-
-���� ����
-n�� 1�̻� 8000000000 ������ �ڿ����Դϴ�.
- */
-
 import java.util.Arrays;
 
 class Solution {
 	public long solution(long n) {
-		long answer = 0;
-
-		String[] arr = Long.toString(n).split("");
-		
+		char[] arr = Long.toString(n).toCharArray();
 		Arrays.sort(arr);
-		
-		for(int i = arr.length-1 ; i>=0 ; i--) {
-			
-			answer = answer*10 + Integer.parseInt(arr[i]);
-		}
-
-		return answer;
+		StringBuilder sb = new StringBuilder(new String(arr));
+		return Long.parseLong(sb.reverse().toString());
 	}
 }
