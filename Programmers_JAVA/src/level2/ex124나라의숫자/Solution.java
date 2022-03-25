512694bb0f424108691b76161079654eaa8e8633
jang <jhbsp@naver.com>
2022-03-25 17:49:11 +0900

    220325 진짜 개멍청인가ㅋㅋㅋㅋㅋ

--- Programmers_JAVA/src/level2/ex124나라의숫자/Solution.java
+++ Programmers_JAVA/src/level2/ex124나라의숫자/Solution.java
@@ -1,28 +1,21 @@
 package level2.ex124나라의숫자;
 
-import java.util.Stack;
-
 public class Solution {
 
 	public String solution(int n) {
 
-		Stack<Integer> stack = new Stack<>();
+		StringBuilder sb = new StringBuilder();
 
 		while (n > 0) {
 			if (n % 3 == 0) {
-				stack.push(4);
+				sb.append(4);
 				n -= 3;
 			} else {
-				stack.push(n % 3);
+				sb.append(n % 3);
 			}
 			n /= 3;
 		}
 
-		StringBuilder sb = new StringBuilder();
-
-		while (!stack.isEmpty())
-			sb.append(stack.pop());
-
-		return sb.toString();
+		return sb.reverse().toString();
 	}
 }
