f27520f87c155026d69bd43f651a5bd7bec7958f
jang <jhbsp@naver.com>
2022-12-12 13:17:06 +0900

    221212

--- Programmers_JAVA/src/level3/ex입국심사/Solution.java
+++ Programmers_JAVA/src/level3/ex입국심사/Solution.java
@@ -2,35 +2,18 @@ package level3.ex입국심사;
 
 class Solution {
 	public long solution(int n, int[] times) {
-
-		long head = 0L;
-		long tail = 1000000000000000000L;
-		long mid = 0L;
-
-		long cnt = 0;
-
+		long head = 0L, tail = 1000000000000000000L, mid;
 		while (tail - head > 1) {
-
 			mid = (head + tail) / 2;
-
-			cnt = 0;
-
+			long cnt = 0;
 			for (int time : times) {
 				cnt += mid / time;
-				if (cnt < 0) {
-					tail = mid;
-					continue;
-				}
-
 			}
-
 			if (cnt < n)
 				head = mid;
 			else
 				tail = mid;
-
 		}
-
 		return tail;
 	}
 
