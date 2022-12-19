1c3369bffbcb0af6669782329e65e8e57b06bc5e
jang <jhbsp@naver.com>
2022-12-19 14:08:04 +0900

    221219 refact

--- Programmers_JAVA/src/level2/ex할인행사/Solution.java
+++ Programmers_JAVA/src/level2/ex할인행사/Solution.java
@@ -6,16 +6,12 @@ class Solution {
 	public int solution(String[] want, int[] number, String[] discount) {
 
 		// 원하는 상품명의 index를 획득하기 위한 map 구성
-		HashMap<String, Integer> itemIdx = new HashMap<>();
-		for (int i = 0; i < want.length; i++) {
-			itemIdx.put(want[i], i);
-		}
+		initItemIndex(want);
 
 		// 첫 10일간의 할인항목 중 원하는 상품 할인수를 카운트
 		int[] dcCnt = new int[want.length];
 		for (int i = 0; i < 10; i++) {
-			if (itemIdx.containsKey(discount[i]))
-				dcCnt[itemIdx.get(discount[i])]++;
+			addArrayValue(dcCnt, indexOf(discount[i]), 1);
 		}
 
 		int answer = 0;
@@ -25,22 +21,37 @@ class Solution {
 
 			// 첫날은 할인cnt 업데이트 하지 않는다
 			if (date > 0) {
-
 				// 다음 할인 품목 cnt추가
 				String nextSale = discount[date + 9];
-				if (itemIdx.containsKey(nextSale))
-					dcCnt[itemIdx.get(nextSale)]++;
+				addArrayValue(dcCnt, indexOf(nextSale), 1);
 				// 마지막 할인품목 cnt제거
 				String lastSale = discount[date - 1];
-				if (itemIdx.containsKey(lastSale))
-					dcCnt[itemIdx.get(lastSale)]--;
+				addArrayValue(dcCnt, indexOf(lastSale), -1);
 			}
-
+			
 			if (isSame(number, dcCnt)) {
 				answer++;
 			}
 		}
 		return answer;
+	}
+
+	HashMap<String, Integer> itemIdx;
+
+	void initItemIndex(String[] array) {
+		itemIdx = new HashMap<>();
+		for (int i = 0; i < array.length; i++) {
+			itemIdx.put(array[i], i);
+		}
+	}
+
+	int indexOf(String item) {
+		return itemIdx.containsKey(item) ? itemIdx.get(item) : -1;
+	}
+
+	void addArrayValue(int[] array, int index, int value) {
+		if (-1 < index && index < array.length)
+			array[index] += value;
 	}
 
 	boolean isSame(int[] number, int[] dcCnt) {
