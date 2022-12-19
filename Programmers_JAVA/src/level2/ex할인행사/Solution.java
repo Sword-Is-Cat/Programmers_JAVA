b6bd50584d68a39db6b1bee8f1671bcb57c2f884
jang <jhbsp@naver.com>
2022-12-19 14:06:34 +0900

    221219 refact

--- Programmers_JAVA/src/level2/ex할인행사/Solution.java
+++ Programmers_JAVA/src/level2/ex할인행사/Solution.java
@@ -5,60 +5,51 @@ import java.util.HashMap;
 class Solution {
 	public int solution(String[] want, int[] number, String[] discount) {
 
+		// 원하는 상품명의 index를 획득하기 위한 map 구성
+		HashMap<String, Integer> itemIdx = new HashMap<>();
+		for (int i = 0; i < want.length; i++) {
+			itemIdx.put(want[i], i);
+		}
+
+		// 첫 10일간의 할인항목 중 원하는 상품 할인수를 카운트
 		int[] dcCnt = new int[want.length];
-		HashMap<String, Integer> idxMap = new HashMap<>();
-
-		for (int i = 0; i < want.length; i++)
-			idxMap.put(want[i], i);
-
-		int days = 0;
-		for (int num : number)
-			days += num;
-
-		days--;
-		
-		for (int i = 0; i < days; i++) {
-			//System.out.println(discount[i]);
-			if (idxMap.containsKey(discount[i]))
-				dcCnt[idxMap.get(discount[i])]++;
+		for (int i = 0; i < 10; i++) {
+			if (itemIdx.containsKey(discount[i]))
+				dcCnt[itemIdx.get(discount[i])]++;
 		}
 
 		int answer = 0;
 
-		for (int date = 0; date + days < discount.length; date++) {
+		// 첫날부터 마지막날까지 순회하며 조건만족 확인 및 할인cnt 업데이트
+		for (int date = 0; date + 9 < discount.length; date++) {
 
-			//System.out.println("when date:"+date);
-			String prod = discount[date + days];
-			if (idxMap.containsKey(prod))
-				dcCnt[idxMap.get(prod)]++;
+			// 첫날은 할인cnt 업데이트 하지 않는다
 			if (date > 0) {
-				String rmvProd = discount[date - 1];
-				if (idxMap.containsKey(rmvProd))
-					dcCnt[idxMap.get(rmvProd)]--;
+
+				// 다음 할인 품목 cnt추가
+				String nextSale = discount[date + 9];
+				if (itemIdx.containsKey(nextSale))
+					dcCnt[itemIdx.get(nextSale)]++;
+				// 마지막 할인품목 cnt제거
+				String lastSale = discount[date - 1];
+				if (itemIdx.containsKey(lastSale))
+					dcCnt[itemIdx.get(lastSale)]--;
 			}
 
-			if (isSame(number, dcCnt))
+			if (isSame(number, dcCnt)) {
 				answer++;
-
+			}
 		}
-
 		return answer;
 	}
 
 	boolean isSame(int[] number, int[] dcCnt) {
-		//System.out.println("CALL");
+		if (number.length != dcCnt.length)
+			return false;
 		for (int i = 0; i < number.length; i++) {
-			
-			//System.out.println("index:"+i+", numVal:"+number[i] +", cnt:"+ dcCnt[i]);
 			if (number[i] != dcCnt[i])
 				return false;
 		}
 		return true;
-	}
-
-	public static void main(String[] args) {
-		new Solution().solution(new String[] { "banana", "apple", "rice", "pork", "pot" }, new int[] { 3, 2, 2, 2, 1 },
-				new String[] { "chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice",
-						"pot", "banana", "apple", "banana" });
 	}
 }
