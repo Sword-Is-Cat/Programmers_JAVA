3efc562626ade87d4a0b0e23f9838f3b28919b99
jang <jhbsp@naver.com>
2022-12-22 08:28:26 +0900

    221222

--- Programmers_JAVA/src/level2/ex올바른괄호/Solution.java
+++ Programmers_JAVA/src/level2/ex올바른괄호/Solution.java
@@ -1,34 +1,12 @@
 package level2.ex올바른괄호;
 
-
-/*
- * ���� ����
-��ȣ�� �ٸ��� ¦�������ٴ� ���� '(' ���ڷ� �������� �ݵ�� ¦��� ')' ���ڷ� ������ �Ѵٴ� ���Դϴ�. 
-
-���� ���
-()() �Ǵ� (())() �� �ùٸ� ��ȣ�Դϴ�.
-)()( �Ǵ� (()( �� �ùٸ��� ���� ��ȣ�Դϴ�.
-'(' �Ǵ� ')' �θ� �̷���� ���ڿ� s�� �־����� ��, ���ڿ� s�� �ùٸ� ��ȣ�̸� true�� return �ϰ�, 
-�ùٸ��� ���� ��ȣ�̸� false�� return �ϴ� solution �Լ��� �ϼ��� �ּ���.
-
-���ѻ���
-���ڿ� s�� ���� : 100,000 ������ �ڿ���
-���ڿ� s�� '(' �Ǵ� ')' �θ� �̷���� �ֽ��ϴ�.
- */
-
 class Solution {
-	boolean solution(String s) {
-		char[] arr = s.toCharArray();
-		int balance = 0;
-		for(int i = 0; i<arr.length ; i++) {
-			if(arr[i] == '(') {
-				balance++;
-			}else {
-				balance--;
-			}
-			if(balance<0)
-				return false;
+	boolean solution(String str) {
+		int cntUnpair = 0;
+		for (char ch : str.toCharArray()) {
+			cntUnpair += ch == '(' ? 1 : -1;
+			if (cntUnpair < 0) return false;
 		}
-		return (balance==0)?true:false;
+		return cntUnpair == 0;
 	}
 }
