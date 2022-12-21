48f2bae59598b91bd520c67f417cde3d0d24e4bd
jang <jhbsp@naver.com>
2022-12-21 13:42:56 +0900

    221221

--- Programmers_JAVA/src/level1/ex모의고사/Solution.java
+++ Programmers_JAVA/src/level1/ex모의고사/Solution.java
@@ -4,19 +4,21 @@ package level1.ex모의고사;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 class Solution {
+
+	final static int MANS = 3;
+
 	public static int[] solution(int[] answers) {
 
-		int[] scores = new int[3];
-		int[][] pattern = new int[3][];
+		int[] scores = new int[MANS];
+		int[][] pattern = new int[MANS][];
 		pattern[0] = new int[] { 1, 2, 3, 4, 5 };
 		pattern[1] = new int[] { 2, 1, 2, 3, 2, 4, 2, 5 };
 		pattern[2] = new int[] { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
 
 		for (int qNo = 0; qNo < answers.length; qNo++) {
-			for (int pttNo = 0; pttNo < pattern.length; pttNo++) {
+			for (int pttNo = 0; pttNo < MANS; pttNo++) {
 				if (answers[qNo] == pattern[pttNo][qNo % pattern[pttNo].length]) {
 					scores[pttNo]++;
 				}
@@ -24,15 +26,12 @@ class Solution {
 		}
 
 		int best = Arrays.stream(scores).max().getAsInt();
+		ArrayList<Integer> list = new ArrayList<>();
 
-		List<Integer> list = new ArrayList<>();
-
-		for (int j = 0; j < 3; j++) {
+		for (int j = 0; j < MANS; j++) {
 			if (best == scores[j])
 				list.add(j + 1);
 		}
-
 		return list.stream().mapToInt(i -> i).toArray();
 	}
-
 }
