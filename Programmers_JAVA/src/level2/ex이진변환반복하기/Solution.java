59dafd056b8bae0f82dd6c7b68a42bfca0c309a0
jang <jhbsp@naver.com>
2022-12-29 14:31:46 +0900

    221229

--- Programmers_JAVA/src/level2/ex이진변환반복하기/Solution.java
+++ Programmers_JAVA/src/level2/ex이진변환반복하기/Solution.java
@@ -1,32 +1,29 @@
 package level2.ex이진변환반복하기;
 
+import java.util.stream.IntStream;
+
 class Solution {
-	
+
 	int changeCnt = 0;
 	int removeZero = 0;
-	
-	public int[] solution(String s) {
-		
-		while(s.length()>1) {
-			s = conBinary(s);
-			changeCnt++;
-		}
-		
-		int[] answer = {changeCnt, removeZero};
-		return answer;
-	}
-	
-	public String conBinary(String original) {
-		
-		//0제거한 str의 길이
-		int conLength = original.replace("0", "").length();
-		
-		//감소한길이 = 제거된 0의 갯수
-		int lengthDecrease = original.length() - conLength;
 
-		removeZero += lengthDecrease;
-		
-		//0을 제거한 str의 길이를 binaryString으로 변환하여 return
+	public int[] solution(String s) {
+
+		while (s.length() > 1) {
+			s = conBinary(s);
+		}
+
+		return new int[] { changeCnt, removeZero };
+	}
+
+	public String conBinary(String original) {
+
+		int oriLength = original.length();
+		int conLength = IntStream.range(0, oriLength).map(i -> original.charAt(i) - '0').sum();
+
+		removeZero += oriLength - conLength;
+		changeCnt++;
+
 		return Integer.toBinaryString(conLength);
 	}
 }
