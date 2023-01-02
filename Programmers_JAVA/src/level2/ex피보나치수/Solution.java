596613d0f8bdd3e08173f7f599806e5f87c645cb
jang <jhbsp@naver.com>
2023-01-02 10:23:20 +0900

    230102

--- Programmers_JAVA/src/level2/ex피보나치수/Solution.java
+++ Programmers_JAVA/src/level2/ex피보나치수/Solution.java
@@ -1,31 +1,15 @@
 package level2.ex피보나치수;
 
-
-/*
-���� ����
-�Ǻ���ġ ���� F(0) = 0, F(1) = 1�� ��, 1 �̻��� n�� ���Ͽ� F(n) = F(n-1) + F(n-2) �� ����Ǵ� �� �Դϴ�.
-�������
-F(2) = F(0) + F(1) = 0 + 1 = 1
-F(3) = F(1) + F(2) = 1 + 1 = 2
-F(4) = F(2) + F(3) = 1 + 2 = 3
-F(5) = F(3) + F(4) = 2 + 3 = 5
-�� ���� �̾����ϴ�.
-2 �̻��� n�� �ԷµǾ��� ��, n��° �Ǻ���ġ ���� 1234567���� ���� �������� �����ϴ� �Լ�, solution�� �ϼ��� �ּ���.
-���� ����
-* n�� 1�̻�, 100000������ �ڿ����Դϴ�.
-*/
-
 class Solution {
-  public int solution(int n) {
-      int[] fibo = new int[n+1];
-      fibo[0] = 0;
-      fibo[1] = 1;
-      
-      for(int i=2 ; i<=n ; i++){
-          fibo[i] = fibo[i-1]+fibo[i-2];
-          fibo[i] = (fibo[i]>=1234567) ? fibo[i]-1234567 : fibo[i];
-      }
-      
-      return fibo[n]%1234567;
-  }
+	public int solution(int n) {
+
+		int[] fibo = new int[n + 1];
+		fibo[1] = 1;
+
+		for (int i = 2; i <= n; i++) {
+			fibo[i] = (fibo[i - 1] + fibo[i - 2]) % 1234567;
+		}
+
+		return fibo[n];
+	}
 }
