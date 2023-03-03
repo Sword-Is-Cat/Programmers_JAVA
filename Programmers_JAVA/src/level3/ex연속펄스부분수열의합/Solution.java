8920241dd0efe21001eaecbe89b24b516f16d5b0
jang <jhbsp@naver.com>
2023-03-03 11:53:59 +0900

    230303

--- Programmers_JAVA/src/level3/ex연속펄스부분수열의합/Solution.java
+++ Programmers_JAVA/src/level3/ex연속펄스부분수열의합/Solution.java
@@ -4,20 +4,20 @@ class Solution {
 	public long solution(int[] sequence) {
 
 		long sum = 0;
-		long oddMinSum = 0, oddMaxGap = -sequence[0];
-		long evenMinSum = 0, evenMaxGap = sequence[0];
+		long maxSum = 0, minGap = sequence[0];
+		long minSum = 0, maxGap = sequence[0];
 
 		for (int i = 0; i < sequence.length; i++) {
 
 			sum += i % 2 == 0 ? sequence[i] : -sequence[i];
 
-			oddMaxGap = Math.max(oddMaxGap, -sum - oddMinSum);
-			evenMaxGap = Math.max(evenMaxGap, sum - evenMinSum);
+			minGap = Math.min(minGap, sum - maxSum);
+			maxGap = Math.max(maxGap, sum - minSum);
 
-			oddMinSum = Math.min(oddMinSum, -sum);
-			evenMinSum = Math.min(evenMinSum, sum);
+			maxSum = Math.max(maxSum, sum);
+			minSum = Math.min(minSum, sum);
 		}
-		
-		return Math.max(oddMaxGap, evenMaxGap);
+
+		return Math.max(-minGap, maxGap);
 	}
 }
