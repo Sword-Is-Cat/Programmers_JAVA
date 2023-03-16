a7a1e237d377264e283825b66cbe306be0b056dd
jang <jhbsp@naver.com>
2023-03-16 15:50:26 +0900

    230316

--- Programmers_JAVA/src/level1/ex문자열내마음대로정렬하기/Solution.java
+++ Programmers_JAVA/src/level1/ex문자열내마음대로정렬하기/Solution.java
@@ -1,44 +1,13 @@
 package level1.ex문자열내마음대로정렬하기;
 
-
-/*
-���� ����
-���ڿ��� ������ ����Ʈ strings��, ���� n�� �־����� ��, �� ���ڿ��� �ε��� n��° ���ڸ� �������� �������� �����Ϸ� �մϴ�.
-���� ��� strings�� [sun, bed, car]�̰� n�� 1�̸� �� �ܾ��� �ε��� 1�� ���� u, e, a�� strings�� �����մϴ�.
-
-���� ����
-strings�� ���� 1 �̻�, 50������ �迭�Դϴ�.
-strings�� ���Ҵ� �ҹ��� ���ĺ����� �̷���� �ֽ��ϴ�.
-strings�� ���Ҵ� ���� 1 �̻�, 100������ ���ڿ��Դϴ�.
-��� strings�� ������ ���̴� n���� Ů�ϴ�.
-�ε��� 1�� ���ڰ� ���� ���ڿ��� ���� �� ���, ���������� �ռ� ���ڿ��� ���ʿ� ��ġ�մϴ�.
-*/
-
-
 import java.util.Arrays;
-import java.util.Comparator;
 
 class Solution {
 	public String[] solution(String[] strings, int n) {
-		String[] answer = {};
-		
+
 		Arrays.sort(strings);
-		Arrays.sort(strings, new Comparator<String>() {
+		Arrays.sort(strings, (a, b) -> Character.compare(a.charAt(n), b.charAt(n)));
 
-			@Override
-			public int compare(String o1, String o2) {
-
-				if (o1.charAt(n) < o2.charAt(n))
-					return -1;
-				else if (o1.charAt(n) > o2.charAt(n))
-					return 1;
-				else
-					return 0;
-
-			}
-
-		});
-
-		return answer;
+		return strings;
 	}
 }
