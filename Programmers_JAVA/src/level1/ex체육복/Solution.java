81592134741ca55d1b466d120c39e21a27ffd91a
jang <jhbsp@naver.com>
2023-04-05 15:04:06 +0900

    230405

--- Programmers_JAVA/src/level1/ex체육복/Solution.java
+++ Programmers_JAVA/src/level1/ex체육복/Solution.java
@@ -3,25 +3,28 @@ package level1.ex체육복;
 class Solution {
 	public int solution(int n, int[] lost, int[] reserve) {
 
-		int answer = 0;
-		int[] have = new int[n];
+		int[] cnts = new int[n];
 
-		for (int i : lost)
-			have[i - 1]--;
+		for (int idx : lost) {
+			cnts[idx - 1]--;
+		}
 
-		for (int i : reserve)
-			have[i - 1]++;
+		for (int idx : reserve) {
+			cnts[idx - 1]++;
+		}
 
-		for (int i = 0; i < n - 1; i++)
-			if (have[i] * have[i + 1] < 0) {
-				have[i] = 0;
-				have[i + 1] = 0;
+		for (int i = 1; i < n; i++) {
+			if (cnts[i] * cnts[i - 1] < 0) {
+				cnts[i] = cnts[i - 1] = 0;
 			}
+		}
 
-		for (int i : have)
-			if (i >= 0)
-				answer++;
+		for (int cnt : cnts) {
+			if (cnt < 0) {
+				n--;
+			}
+		}
 
-		return answer;
+		return n;
 	}
 }
