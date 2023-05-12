8b1f1b12072bbb952200b4b2827d0d9d05a4c7cd
jang <jhbsp@naver.com>
2023-05-12 09:23:54 +0900

    230512

--- Programmers_JAVA/src/level1/ex이상한문자만들기/Solution.java
+++ Programmers_JAVA/src/level1/ex이상한문자만들기/Solution.java
@@ -1,43 +1,23 @@
 package level1.ex이상한문자만들기;
 
-/*
- * ���� ����
-���ڿ� s�� �� �� �̻��� �ܾ�� �����Ǿ� �ֽ��ϴ�. �� �ܾ�� �ϳ� �̻��� ���鹮�ڷ� ���еǾ� �ֽ��ϴ�. 
-�� �ܾ��� ¦����° ���ĺ��� �빮�ڷ�, Ȧ����° ���ĺ��� �ҹ��ڷ� �ٲ� ���ڿ��� �����ϴ� �Լ�, 
-solution�� �ϼ��ϼ���.
-
-���� ����
-���ڿ� ��ü�� ¦/Ȧ�� �ε����� �ƴ϶�, �ܾ�(������ ����)���� ¦/Ȧ�� �ε����� �Ǵ��ؾ��մϴ�.
-ù ��° ���ڴ� 0��° �ε����� ���� ¦����° ���ĺ����� ó���ؾ� �մϴ�.
- */
-
 class Solution {
 	public String solution(String s) {
-		String answer = "";
 
-		char[] arr = s.toCharArray();
 		StringBuilder sb = new StringBuilder();
 
-		for (int i = 0; i < arr.length; i++) {
+		boolean isUpper = true;
+		char blank = ' ';
 
-			if (arr[i] != ' ') {
+		for (char ch : s.toCharArray()) {
 
-				if (i == 0) {
-					arr[i] = Character.toUpperCase(arr[i]);
-					
-				} else if (Character.isUpperCase(arr[i - 1])) {
-					arr[i] = Character.toLowerCase(arr[i]);
-					
-				} else {
-					arr[i] = Character.toUpperCase(arr[i]);
-				}
-
+			if (ch == blank) {
+				isUpper = true;
+				sb.append(ch);
+			} else {
+				sb.append(isUpper ? Character.toUpperCase(ch) : Character.toLowerCase(ch));
+				isUpper = !isUpper;
 			}
-			sb.append(arr[i]);
 		}
-
-		answer = sb.toString();
-		return answer;
+		return sb.toString();
 	}
-
 }
