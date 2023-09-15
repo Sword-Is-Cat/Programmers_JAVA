69b74fd35d9f37ba0445bfc5ee91f34182a4dc74
jang <jhbsp@naver.com>
2023-09-15 14:58:29 +0900

    230915

--- Programmers_JAVA/src/level2/ex괄호변환/Solution.java
+++ Programmers_JAVA/src/level2/ex괄호변환/Solution.java
@@ -1,80 +1,62 @@
 package level2.ex괄호변환;
 
-
-/*
- * https://programmers.co.kr/learn/courses/30/lessons/60058
- */
-
 class Solution {
-
 	public String solution(String p) {
-		
-		if(p.length()==0)
-			return "";
-		
+
 		StringBuilder sb = new StringBuilder();
-		String u = getBalanced(p);
-		p = p.substring(u.length());
-		
-		if(isRight(u)) {
-			sb.append(u);
-			sb.append(solution(p));
-		}else {
-			sb.append("(");
-			sb.append(solution(p));
-			sb.append(")");
-			sb.append(getReverse(u));
+
+		// 1. 입력이 빈 문자열인 경우, 빈 문자열을 반환합니다.
+		if (p.isEmpty())
+			return sb.toString();
+
+		// 2. 문자열 w를 두 "균형잡힌 괄호 문자열" u, v로 분리합니다.
+		// 단, u는 "균형잡힌 괄호 문자열"로 더 이상 분리할 수 없어야 하며, v는 빈 문자열이 될 수 있습니다.
+		String u = getBalancedPrefix(p);
+		String v = p.substring(u.length());
+
+		if (isRightString(u)) {
+			// 3. 문자열 u가 "올바른 괄호 문자열" 이라면 문자열 v에 대해 1단계부터 다시 수행합니다.
+			// 3-1. 수행한 결과 문자열을 u에 이어 붙인 후 반환합니다.
+			sb.append(u).append(solution(v));
+		} else {
+			sb // 4. 문자열 u가 "올바른 괄호 문자열"이 아니라면 아래 과정을 수행합니다.
+					.append('(') // 4-1. 빈 문자열에 첫 번째 문자로 '('를 붙입니다.
+					.append(solution(v)) // 4-2. 문자열 v에 대해 1단계부터 재귀적으로 수행한 결과 문자열을 이어 붙입니다.
+					.append(')') // 4-3. ')'를 다시 붙입니다.
+					.append(getReflectString(u.substring(1, u.length() - 1)));
+			// 4-4. u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다.
+			// 4-5. 생성된 문자열을 반환합니다.
 		}
 
 		return sb.toString();
 	}
 
-	public String getBalanced(String str) {
-
-		int balance = 0;
-		int i = 0;
+	private String getBalancedPrefix(String str) {
+		int balance = 0, index = 0;
 		do {
-			if (str.substring(i++, i).equals("(")) {
-				balance++;
-			} else {
-				balance--;
-			}
-		} while (balance != 0);
-
-		return str.substring(0, i);
+			char ch = str.charAt(index++);
+			balance += ch == '(' ? 1 : -1;
+		} while (balance != 0 && index < str.length());
+		return str.substring(0, index);
 	}
 
-	public boolean isRight(String str) {
-		
-		int balance = 0;
-		int i = 0;
-		while (!(balance < 0)) {
-			if (str.substring(i++, i).equals("(")) {
-				balance++;
-			} else {
-				balance--;
-			}
-			if(i == str.length())
-				return true;
+	private boolean isRightString(String str) {
+		int opened = 0;
+		for (char ch : str.toCharArray()) {
+			if (ch == '(')
+				opened++;
+			else if (opened > 0)
+				opened--;
 		}
-		return false;
+		return opened == 0;
 	}
-	
-	public String getReverse(String str) {
-		
-		char[] arr = (str.substring(1, str.length()-1)).toCharArray();
-		if(arr.length == 0)
-			return "";
-		
-		StringBuilder sb = new StringBuilder();
-		
-		for(char ch : arr) {
-			if(ch == '(')
-				sb.append(')');
-			else
-				sb.append('(');
+
+	private String getReflectString(String str) {
+		char[] arr = str.toCharArray();
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] ^= '(' ^ ')';
 		}
-		
-		return sb.toString();
+		return new String(arr);
 	}
+
 }
